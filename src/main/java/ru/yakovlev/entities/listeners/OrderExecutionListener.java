41fc4e8e6b6942531c8b651c38436dcbdbd7d6ce
Alexander Yakovlev <sanyakovlev@yandex.ru>
2020-03-10 15:58:39 +0700

    feat: implemented OrderExecutionProcess

---
+++ src/main/java/ru/yakovlev/entities/listeners/OrderExecutionListener.java
@@ -0,0 +1,52 @@
+package ru.yakovlev.entities.listeners;
+
+import java.util.Objects;
+import javax.persistence.PostPersist;
+import lombok.NoArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.transaction.support.TransactionSynchronization;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+import ru.yakovlev.entities.OrderExecution;
+import ru.yakovlev.service.OrderExecutionProcess;
+
+/**
+ * Order execution entity event listener.
+ *
+ * @author Yakovlev Aleander (sanyakovlev@yandex.ru)
+ * @since 0.6.0
+ */
+@NoArgsConstructor
+public class OrderExecutionListener {
+    private Listener listener;
+
+    @PostPersist
+    public void afterPersist(OrderExecution orderExecution) {
+        this.init();
+        this.listener.afterPersist(orderExecution);
+    }
+
+    private void init() {
+        if (Objects.isNull(this.listener)) {
+            this.listener = new Listener();
+        }
+    }
+
+    @Configurable
+    public static class Listener {
+        @Autowired
+        private OrderExecutionProcess orderExecutionProcess;
+
+        public void afterPersist(OrderExecution orderExecution) {
+            TransactionSynchronizationManager.registerSynchronization(new TransactionSynchronization() {
+                @SneakyThrows
+                @Override
+                public void afterCommit() {
+                    orderExecutionProcess.execute(orderExecution);
+                }
+            });
+        }
+
+    }
+}
