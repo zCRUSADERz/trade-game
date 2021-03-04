fd6b9ad31d36dc7bf45ba254cb7ea96ef9d444f5
Alexander Yakovlev <sanyakovlev@yandex.ru>
2021-03-04 17:43:31 +0700

    feat: implemented SecurityService

---
+++ src/main/java/ru/yakovlev/service/security/SecurityService.java
@@ -0,0 +1,67 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2021 Yakovlev Alexander
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package ru.yakovlev.service.security;
+
+import lombok.AllArgsConstructor;
+import lombok.val;
+import org.springframework.security.acls.domain.BasePermission;
+import org.springframework.security.acls.domain.CumulativePermission;
+import org.springframework.security.acls.domain.GrantedAuthoritySid;
+import org.springframework.security.acls.domain.ObjectIdentityImpl;
+import org.springframework.security.acls.model.MutableAclService;
+import org.springframework.stereotype.Service;
+
+/**
+ * Security service.
+ *
+ * @author Yakovlev Aleander (sanyakovlev@yandex.ru)
+ * @since 0.3.0
+ */
+@Service
+@AllArgsConstructor
+public class SecurityService {
+    private final MutableAclService aclService;
+
+    /**
+     * Create access control list for given entity.
+     *
+     * @param entity entity.
+     */
+    public void createAcl(Object entity) {
+        val identity = new ObjectIdentityImpl(entity);
+        val acl = this.aclService.createAcl(identity);
+        val ownerSid = acl.getOwner();
+        val ownerPermission = new CumulativePermission();
+        ownerPermission.set(BasePermission.READ);
+        ownerPermission.set(BasePermission.WRITE);
+        ownerPermission.set(BasePermission.CREATE);
+        ownerPermission.set(BasePermission.DELETE);
+        ownerPermission.set(BasePermission.ADMINISTRATION);
+        acl.insertAce(acl.getEntries().size(), ownerPermission, ownerSid, true);
+        acl.insertAce(acl.getEntries().size(), BasePermission.READ, new GrantedAuthoritySid("ROLE_SUPERVISOR"), true);
+        this.aclService.updateAcl(acl);
+    }
+
+}
